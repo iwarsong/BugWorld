ff3d466416e7f838dbd0a78237ec2a31d17b191c
retamia <liumengte@howzhi.com>
2015-10-25 03:13:55 +0800

    详情键盘事件

--- web/js/default/main.js
+++ web/js/default/main.js
@@ -56,4 +56,63 @@ define(function(require, exports, module) {
         searchIssues.search(method, getUrl(), searchIssues.callback);
     }
 
+    var IssueModal = function(){
+        this.grid;
+        this.setGrid = function($issueGrid){this.grid = $issueGrid};
+        this.getGrid = function(){return this.grid};
+        this.getLastGrid = function(){
+            if (this.getGrid() === undefined){
+                return;
+            }
+            return this.getGrid().parent().prev().children('.issue-grid');
+        };
+        this.getNextGrid = function(){
+            if (this.getGrid() === undefined){
+                return;
+            }
+            return this.getGrid().parent().next().children('.issue-grid');
+        }
+    };
+
+    var Keypress = require('keypress');
+    var issueModal = new IssueModal();
+
+    $('.issue-image').on('click', function(){
+        var $this = $(this);
+        var $issueGrid = $this.parent('.issue-grid');
+        issueModal.setGrid($issueGrid);
+
+        var listener = new Keypress.Listener();
+        listener.simple_combo('left', function(){
+            var lastGrid = issueModal.getLastGrid();
+            if(lastGrid.length == 0){
+                return ;
+            }
+
+            issueModal.setGrid(lastGrid);
+            var $grid = issueModal.getGrid();
+            $.get($grid.children('.issue-image').data('url'), {id:$grid.data('id')}, function(html){
+                $('#issue-show-modal').html(html);
+            });
+        });
+
+        listener.simple_combo('right', function(){
+            var nextGrid = issueModal.getNextGrid();
+            if(nextGrid.length == 0){
+                return ;
+            }
+
+            issueModal.setGrid(nextGrid);
+            var $grid = issueModal.getGrid();
+            $.get($grid.children('.issue-image').data('url'), {id:$grid.data('id')}, function(html){
+                $('#issue-show-modal').html(html);
+            });
+        });
+    });
+
+
+
+
+
+
 });
