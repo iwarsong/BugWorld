90f56beab0b5ac25b25a8eaf63019ee2636fa8d1
Wellming Li <wellming.li@howzhi.com>
2015-10-25 03:19:03 +0800

    issue create

--- web/js/default/index.js
+++ web/js/default/index.js
@@ -10,70 +10,9 @@ define(function(require, exports, module) {
     require('./userlist-floatbar.js');
     require('./main.js');
 
+    require('./issue-create.js');
+
     exports.run = function() {
-
-        var $modal = $('#issue-create-modal');
-
-        $('#issue-create-modal .issue-image').pastableNonInputable();
-
-        $('#issue-create-modal .issue-image')
-
-
-        $('#issue-create-modal').on('shown.bs.modal', function () {
-            console.log('show');
-            var html = "";
-            html += '<div class="issue-image" tabindex="1"><div class="empty-text">请粘贴图片</div></div>';
-            $modal.find('.issue-image-box').html(html);
-
-            $modal.find('.issue-image').pastableNonInputable();
-
-            $modal.find('.issue-image').on('pasteImage', function(ev, data) {
-                console.log('paste');
-
-                var blobUrl = URL.createObjectURL(data.blob);
-
-                var html = "";
-                html += '<a href="' + blobUrl + '" target="_blank" class="image-link">';
-                html += '  <img src="' + data.dataURL +'" class="img-responsive">';
-                html += '</a>';
-
-                $(this).html(html);
-
-            });
-
-        });
-
-        $('#issue-create-modal').on('hide.bs.modal', function () {
-            console.log('hide');
-            $modal.find('.issue-image').remove();
-            $modal.find('textarea').val('');
-
-        });
-
-        // $('*').pastableNonInputable();
-
-        // $('*').on('pasteImage', function(ev, data) {
-        //     console.log("dataURL: " + data.dataURL);
-        //     console.log("width: " + data.width);
-        //     console.log("height: " + data.height);
-        //     console.log(data.blob);
-
-        //     var fd = new FormData();
-        //     fd.append('image', data.blob);
-
-        //     $.ajax({
-        //       url :  "/image/upload",
-        //       type: 'POST',
-        //       data: fd,
-        //       contentType: false,
-        //       processData: false,
-        //       success: function(data) {
-        //         console.log('success');
-        //       },    
-        //       error: function() {
-        //         console.log('error');
-        //       }
-        //     });
 
 
         // }).on('pasteText', function(ev, data) {
